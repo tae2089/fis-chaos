1028d22951af3d0dcb807cd3ac9df1734c15159c
bob(taebinlim) <bob@bc-labs.net>
2023-06-14 10:36:36 +0900

    feature: :seedling: create  domain,models,entity for fis and StressChaos

---
+++ domain/fis.go
@@ -0,0 +1,12 @@
+package domain
+
+import "context"
+
+type Fis struct {
+	Sec            int    `json:"sec" binding:"required"`
+	ExperimentName string `json:"experimentName" binding:"required"`
+}
+
+type FisUsecase interface {
+	CreateStressChaos(ctx context.Context, stressChaos StressChaosDto) (string, error)
+}
