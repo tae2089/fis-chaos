ba64208b6330d61d00ea1b2f7bcae61ba32c4105
bob(taebinlim) <bob@bc-labs.net>
2023-06-14 10:37:30 +0900

    feature: :seedling: update main code for running server

--- run/main.go
+++ run/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/tae2089/fis-chaos/api/router"
+	"github.com/tae2089/fis-chaos/config"
 )
 
 func main() {
-	r := gin.Default()
-	r.Run()
+	config.LoadEnv()
+	timeout := time.Duration(3) * time.Second
+	gin := gin.Default()
+	router.SetUp(timeout, gin)
+	gin.Run(":8080")
 }
