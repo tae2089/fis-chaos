b542edb29ba5a1fbbda9f5714678589697a247f2
tae2089 <tae2089002@gmail.com>
2023-06-15 10:10:18 +0900

    feature: :seedling: update mode type that string to ChaosMeshMode

--- domain/pod.go
+++ domain/pod.go
@@ -1,9 +1,12 @@
 package domain
 
+import "github.com/tae2089/fis-chaos/types"
+
 type PodStressSpec struct {
-	Action         string         `json:"action" binding:"required"`
-	ContainerNames []string       `json:"containerNames" binding:"required"`
-	GracePeriod    int64          `json:"gracePeriod,omitempty"`
-	Mode           string         `json:"mode" binding:"required"`
-	Selector       ChaosSelectors `json:"selector" binding:"required"`
+	Action         string              `json:"action" binding:"required"`
+	ContainerNames []string            `json:"containerNames" binding:"required"`
+	GracePeriod    int64               `json:"gracePeriod,omitempty"`
+	Mode           types.ChaosMeshMode `json:"mode" binding:"required"`
+	Selector       ChaosSelectors      `json:"selector" binding:"required"`
+	Value          string              `json:"value,omitempty"`
 }
