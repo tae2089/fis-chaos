e62885ddbc3d0246646335d9a7c3ac157d457c94
tae2089 <tae2089002@gmail.com>
2023-06-15 14:56:55 +0900

    feature: :seedling: change struct name PodStressSpec to PodChaosDto (#3)

--- domain/pod.go
+++ domain/pod.go
@@ -1,12 +1,37 @@
 package domain
 
-import "github.com/tae2089/fis-chaos/types"
+import (
+	"encoding/json"
 
-type PodStressSpec struct {
+	"github.com/tae2089/fis-chaos/types"
+)
+
+type PodChaosDto struct {
+	Fis          `json:"fis" binding:"required"`
+	PodChaosSpec `json:"chaosSpec"`
+}
+
+type PodChaosSpec struct {
 	Action         string              `json:"action" binding:"required"`
 	ContainerNames []string            `json:"containerNames" binding:"required"`
 	GracePeriod    int64               `json:"gracePeriod,omitempty"`
 	Mode           types.ChaosMeshMode `json:"mode" binding:"required"`
 	Selector       ChaosSelectors      `json:"selector" binding:"required"`
 	Value          string              `json:"value,omitempty"`
+}
+
+func (d PodChaosDto) GetSec() int {
+	return d.Sec
+}
+
+func (d PodChaosDto) GetExperimentName() string {
+	return d.ExperimentName
+}
+
+func (d PodChaosDto) GetSpec() string {
+	jsonbinary, err := json.Marshal(d.PodChaosSpec)
+	if err != nil {
+		return ""
+	}
+	return string(jsonbinary)
 }
